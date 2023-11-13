257800d14be525d2be2f05551660155d774a2007
yaetoti <turkmencheburek0.o@gmail.com>
2023-11-13 17:20:52 +0200

    Add working installer dialog.

--- Main.cpp
+++ Main.cpp
@@ -13,15 +13,6 @@
 
 #include "resource.h"
 
-// TODO: Dialog forms
-// Button "Select Folder"
-// Static Text "Path"
-
-// Edit Box "Filename" + Static Text
-// Edit Box "Registry Key" + Static Text
-
-// Button "Install" (Disabled)
-
 bool InstallResource(const std::wstring& folderPath, const std::wstring& fileName, LPCWSTR lpResourceName, LPCWSTR lpResourceType) {
     if (!CreateDirectoryW(folderPath.c_str(), nullptr) && GetLastError() != ERROR_ALREADY_EXISTS) {
         return false;
@@ -61,37 +52,112 @@ std::wstring BrowseFolder() {
     return L"";
 }
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int) {
-    std::wstring path = BrowseFolder();
-    std::wstring filename = L"Application";
-    Console::GetInstance()->WPrintF(L"%s\n", path.c_str());
-    if (path.empty()) {
-        return 1;
+struct InstallerInput {
+    std::wstring path;
+    std::wstring registryKey;
+    std::wstring name;
+};
+
+LRESULT CALLBACK InstallerProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_INITDIALOG:
+        SetWindowLongPtrW(hwnd, GWLP_USERDATA, lParam);
+        SetDlgItemTextW(hwnd, ID_INSTALLER_TEXT_DIRECTORY, L"");
+        break;
+    case WM_CLOSE:
+        EndDialog(hwnd, FALSE);
+        break;
+    case WM_COMMAND:
+    {
+        auto input = (InstallerInput*)GetWindowLongPtrW(hwnd, GWLP_USERDATA);
+
+        if (LOWORD(wParam) == ID_INSTALLER_BUTTON_FOLDER && HIWORD(wParam) == BN_CLICKED) {
+            std::wstring path = BrowseFolder();
+            if (!path.empty()) {
+                SetDlgItemTextW(hwnd, ID_INSTALLER_TEXT_DIRECTORY, path.c_str());
+                input->path = std::move(path);
+            }
+
+            HWND hRegistry = GetDlgItem(hwnd, ID_INSTALLER_EDIT_REGISTRY);
+            HWND hName = GetDlgItem(hwnd, ID_INSTALLER_EDIT_NAME);
+            HWND hButton = GetDlgItem(hwnd, ID_INSTALLER_BUTTON_INSTALL);
+            int registryLength = GetWindowTextLengthW(hRegistry);
+            int nameLength = GetWindowTextLengthW(hName);
+            if (registryLength == 0 || nameLength == 0 || input->path.empty()) {
+                EnableWindow(hButton, FALSE);
+            }
+            else {
+                EnableWindow(hButton, TRUE);
+            }
+        }
+        else if ((LOWORD(wParam) == ID_INSTALLER_EDIT_REGISTRY || LOWORD(wParam) == ID_INSTALLER_EDIT_NAME)
+            && HIWORD(wParam) == EN_CHANGE) {
+            // Too lazy to put this code into separate functions but not so lazy to write a long-ass comment about that
+            HWND hRegistry = GetDlgItem(hwnd, ID_INSTALLER_EDIT_REGISTRY);
+            HWND hName = GetDlgItem(hwnd, ID_INSTALLER_EDIT_NAME);
+            HWND hButton = GetDlgItem(hwnd, ID_INSTALLER_BUTTON_INSTALL);
+            int registryLength = GetWindowTextLengthW(hRegistry);
+            int nameLength = GetWindowTextLengthW(hName);
+            if (registryLength == 0 || nameLength == 0 || input->path.empty()) {
+                EnableWindow(hButton, FALSE);
+            }
+            else {
+                EnableWindow(hButton, TRUE);
+            }
+        }
+        else if (LOWORD(wParam) == ID_INSTALLER_BUTTON_INSTALL && HIWORD(wParam) == BN_CLICKED) {
+            HWND hRegistry = GetDlgItem(hwnd, ID_INSTALLER_EDIT_REGISTRY);
+            HWND hName = GetDlgItem(hwnd, ID_INSTALLER_EDIT_NAME);
+            int registryLength = GetWindowTextLengthW(hRegistry);
+            int nameLength = GetWindowTextLengthW(hName);
+            input->registryKey.resize(registryLength);
+            input->name.resize(registryLength);
+            GetDlgItemTextW(hwnd, ID_INSTALLER_EDIT_REGISTRY, &input->registryKey[0], registryLength + 1);
+            GetDlgItemTextW(hwnd, ID_INSTALLER_EDIT_NAME, &input->name[0], nameLength + 1);
+            EndDialog(hwnd, TRUE);
+        }
+
+        break;
+    }
+    default:
+        return FALSE;
     }
 
-    /*
-	auto info = std::make_unique<GatheredInfo>(L"C:\\MyProg");
-	Console::GetInstance()->WPrintF(L"%s\n", info->ToString().c_str());
+    return TRUE;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int) {
+    auto installerData = std::make_unique<InstallerInput>();
+    if (!DialogBoxParamW(hInstance, MAKEINTRESOURCE(ID_DIALOG_INSTALLER), nullptr, InstallerProc, (LPARAM)installerData.get())) {
+        return 0;
+    }
+    // Write gathered data to registry
+	auto info = std::make_unique<GatheredInfo>(installerData->path);
 	std::vector<uint8_t> data = info->Serialize();
-    RegWriteBinaryData(HKEY_CURRENT_USER, L"Kostin A.S.", L"GatheredInfo", data);
-    
+    if (ERROR_SUCCESS != RegWriteBinaryData(HKEY_CURRENT_USER, installerData->registryKey, L"GatheredInfo", data)) {
+        MessageBoxW(nullptr, L"Error during license generation!", L"Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+    // Generate license file
     std::unique_ptr<Signature> signature(CreateDigitalSignature(data));
     if (!signature) {
-        Console::GetInstance()->WPrintF(L"SIG_ERR\n");
+        MessageBoxW(nullptr, L"Error during license generation!", L"Error", MB_OK | MB_ICONERROR);
         return 1;
     }
 
     {
-        std::ofstream file(L"license", std::ios::trunc | std::ios::binary);
+        std::ofstream file(installerData->path + L"\\license", std::ios::trunc | std::ios::binary);
         file << signature->signature.size();
         file.write((const char*)signature->signature.data(), signature->signature.size());
         file << signature->key.size();
         file.write((const char*)signature->key.data(), signature->key.size());
     }
-    */
 
-    InstallResource(path, filename + L".exe", MAKEINTRESOURCEW(IDR_RT_APPLICATION1), L"RT_APPLICATION");
+    if (!InstallResource(installerData->path, installerData->name + L".exe", MAKEINTRESOURCEW(IDR_RT_APPLICATION1), L"RT_APPLICATION")) {
+        MessageBoxW(nullptr, L"Error during program installation!", L"Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
 
-	Console::GetInstance()->Pause();
-	return 0;
+    MessageBoxW(nullptr, L"Program was installed successfully!", L"Information", MB_OK | MB_ICONINFORMATION);
+    return 0;
 }
--- resource.h
+++ resource.h
@@ -3,14 +3,20 @@
 // Used by PR3.rc
 //
 #define IDR_RT_APPLICATION1             102
+#define ID_DIALOG_INSTALLER             103
+#define ID_INSTALLER_BUTTON_FOLDER      1001
+#define ID_INSTALLER_EDIT_REGISTRY      1002
+#define ID_INSTALLER_EDIT_NAME          1003
+#define ID_INSTALLER_TEXT_DIRECTORY     1004
+#define ID_INSTALLER_BUTTON_INSTALL     1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
